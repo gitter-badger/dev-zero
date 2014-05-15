d9c2219ca966251b87d9c1d3465bd40b443a13ae
martijnreening <m.reening@ziggo.nl>
2014-05-15 18:09:30 +0200

    Initial commit

---
+++ bot.js
@@ -0,0 +1,15 @@
+var config = require('./config.json');
+
+var irc = require('irc');
+var client = new irc.Client(config.server, config.nick, {
+    channels: config.channels,
+});
+
+client.addListener('error', function(message) {
+    console.log('error: ', message);
+});
+
+var Datastore = require('nedb');
+var db = new Datastore({ filename: config.datafile, autoload: true });
+
+require('./modules/automode.js')(client, db);
