1808748e9cab0f3317922585bc113209d8f47087
jk-5 <jeffreykog@me.com>
2014-06-02 18:22:13 +0200

    Added more events

--- modules/commithook.js
+++ modules/commithook.js
@@ -62,6 +62,55 @@ module.exports = function(client, config) {
 			        		str = str + irc.colors.wrap('orange', c.id.substr(0, 7)) + " - " + c.message.replace(/\+/g, ' ');
 			        		client.say("#server", str);
 			        	}
+		        	}else if(e == "ping"){
+			        	var str = irc.colors.wrap('gray', '[' + reponame + '] ') + irc.colors.wrap('cyan', '[GitHub] ') + data.zen;
+			        	client.say("#server", str);
+		        	}else if(e == "issues"){
+			        	var str = irc.colors.wrap('gray', '[' + reponame + '] ');
+			        	str = str + irc.colors.wrap('cyan', data.sender.login) + ' ' + data.action + ' issue ' + irc.colors.wrap('orange', '#' + data.issue.number) + ': ' + data.issue.title + ' - ' + data.issue.html_url;
+			        	client.say("#server", str);
+		        	}else if(e == "issue_comment"){
+			        	var str = irc.colors.wrap('gray', '[' + reponame + '] ');
+			        	str = str + irc.colors.wrap('cyan', data.sender.login) + ' commented on issue ' + irc.colors.wrap('orange', '#' + data.issue.number) + ': ' + data.issue.title + ' - ' + data.comment.html_url;
+			        	client.say("#server", str);
+		        	}else if(e == "commit_comment"){
+			        	var str = irc.colors.wrap('gray', '[' + reponame + '] ');
+			        	str = str + irc.colors.wrap('cyan', data.sender.login) + ' commented on commit ' + irc.colors.wrap('orange', data.commit_id.substr(0, 7)) + ' - ' + data.comment.html_url;
+			        	client.say("#server", str);
+		        	}else if(e == "create"){
+			        	var str = irc.colors.wrap('gray', '[' + reponame + '] ');
+			        	str = str + irc.colors.wrap('cyan', data.sender.login) + ' created ' + data.ref_type;
+			        	if(data['ref']){
+			        		str = str + ' ' + irc.colors.wrap('orange', data.ref);
+			        	}
+			        	str = str + ' - ' + data.repository.url;
+			        	client.say("#server", str);
+		        	}else if(e == "delete"){
+			        	var str = irc.colors.wrap('gray', '[' + reponame + '] ');
+			        	str = str + irc.colors.wrap('cyan', data.sender.login) + ' deleted ' + data.ref_type + ' ' + irc.colors.wrap('orange', data.ref) + ' - ' + data.repository.html_url;
+			        	client.say("#server", str);
+		        	}else if(e == "pull_request"){
+			        	var str = irc.colors.wrap('gray', '[' + reponame + '] ');
+			        	str = str + irc.colors.wrap('cyan', data.sender.login) + ' ' + data.action ' pull request ' + irc.colors.wrap('orange', '#' + data.num) + ': ' + data.title + ' - ' + data.pull_request.html_url;
+			        	client.say("#server", str);
+		        	}else if(e == "pull_request_review_comment"){
+			        	var str = irc.colors.wrap('gray', '[' + reponame + '] ');
+			        	str = str + irc.colors.wrap('cyan', data.sender.login) + ' reviewed pull request ' + irc.colors.wrap('orange', '#' + data.num) + ' commit - ' + data.comment.html_url;
+			        	client.say("#server", str);
+		        	}else if(e == "gollum"){
+			        	client.say("#server", "Someone updated a wiki. I don't know how to format this yet. Please go bug jk-5 about this");
+		        	}else if(e == "watch"){
+			        	var str = irc.colors.wrap('gray', '[' + reponame + '] ');
+			        	str = str + irc.colors.wrap('cyan', data.sender.login) + ' starred ' + irc.colors.wrap('cyan', data.repository.name) + ' - ' + data.sender.html_url;
+			        	client.say("#server", str);
+		        	}else if(e == "release"){
+			        	var str = irc.colors.wrap('gray', '[' + reponame + '] ');
+			        	str = str + irc.colors.wrap('cyan', data.sender.login) + ' ' + data.action + ' ' + irc.colors.wrap('cyan', data.release.tag_name + ' | ' + data.release.name) + ' - ' + data.release.html_url;
+			        	client.say("#server", str);
+		        	}else if(e == "fork"){
+			        	var str = irc.colors.wrap('gray', '[' + reponame + '] ');
+			        	str = str + irc.colors.wrap('cyan', data.sender.login) + ' forked the repository - ' + data.forkee.owner.html_url;
+			        	client.say("#server", str);
 		        	}
 	        	}
 	        });
