f00b995a4eee63f40a40ff3cf35dcb1dfc47e593
jk-5 <jk-5@users.noreply.github.com>
2014-09-18 19:36:36 +0200

    Strim weapon and user names

--- modules/equip.js
+++ modules/equip.js
@@ -1,11 +1,13 @@
 var players = {};
 
+String.prototype.trim=function(){return this.replace(/^\s+|\s+$/g, '');};
+
 module.exports = function(client, db) {
     client.addListener('message', function(from, to, message) {
         if (!/^!equip /.test(message))
             return;
 
-        var weapon = message.substr(7);
+        var weapon = message.substr(7).trim();
         players[from] = weapon
 
         client.say(to, from + ' equips ' + weapon);
@@ -16,7 +18,7 @@ module.exports = function(client, db) {
             return;
 
         var damage = Math.round(Math.random() * 20 + 5);
-        var who = message.substr(8);
+        var who = message.substr(8).trim();
         var weapon = players[from];
 
         client.say(to, from + ' attacks ' + who + ' with ' + weapon + ' for ' + damage + ' points of damage');
